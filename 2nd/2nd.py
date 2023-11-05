0a5db53644e7b1681804d994f9da89a9bf1c678e
Ramachandra-2k96 <149596008+Ramachandra-2k96@users.noreply.github.com>
2023-11-05 22:09:55 +0530

    Create 2nd.py

---
+++ 2nd/2nd.py
@@ -0,0 +1,17 @@
+import mod_perm as mp
+
+
+list1 = mp.perm3('EAT')
+print(list1)
+list1 = mp.perm4('FAST')
+print(list1)
+list1 = mp.perm5('SMILE')
+print(list1)
+list1 = mp.perm6('FASTER')
+print(list1)
+list1 = mp.perm7('SINCERE')
+print(list1)
+list1 = mp.perm8('PROGRESS')
+print(list1)
+list1 = mp.perm9('BEAUTIFUL')
+print(list1)
