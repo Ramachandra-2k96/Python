177b490b623410d35d7c4489023f4479cdbab72a
Ramachandra-2k96 <149596008+Ramachandra-2k96@users.noreply.github.com>
2023-11-17 22:11:02 +0530

    Update diffchecker.py
    Update diffchecker.py

--- 10th/diffchecker.py
+++ 10th/diffchecker.py
@@ -27,18 +27,18 @@ def diff2(fname1, fname2):
         lineno = difflines[j]
         list5 = list3[j].split(" ")
         list6 = list4[j].split(" ")
-        len5 = max(len(list5), len(list6)) 
-        count = 0
+        len5 = max(len(list5), len(list6)) #consider the maxmimum length
+        count = 0  #marker for starting position of mismatch
         line_diff = "line "+str(lineno) +" mismatch "
         for i in range(0, len5, 1):
-            word1 = list5[i] if i < len(list5) else ""
+            word1 = list5[i] if i < len(list5) else "" #if the i is less than the length then append the word else append empty
             word2 = list6[i] if i < len(list6) else ""
             
-            if word1 != word2:
-                if count == 0:
+            if word1 != word2:   #check for equal
+                if count == 0: #if the mismatch is happening for the first time then add word place and 2 words
                     line_diff += " word "+str(i)+" "+word1+" : "+word2 
                     count+=1 
-                else:
+                else: #if the mismatch has already occured and there is not word to proceed in any 1 of the line then add the remainnig word
                      line_diff += word1+"  "+word2    
                      
         answers.append(line_diff)
