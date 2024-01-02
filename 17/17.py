2977baa338da5c4287e55f3679e0025b9f618897
Ramachandra-2096 <ramachandraudupa2004@gmail.com>
2024-01-02 06:58:16 +0530

    updated code

--- 17/17.py
+++ 17/17.py
@@ -1,10 +1,15 @@
 import csv
-state = input("Enter state-code :").upper().strip()
-maps ={}
+list1 =[]
 with open("Holidays_2024.csv", 'r') as file:
     csv_reader = csv.reader(file)
     for row in csv_reader:
-        maps[row[0]] = [row[1], row[2], row[3].replace("\n", " ").replace('&',',')]
-for d,value in maps.items():
-    if state in value[2] and "except" not in value[2]:
-        print(d,value[0],", Occasion :",value[1])
+        list1 .append([row[0],row[1], row[2], row[3].replace("\n", " ").replace('&',',')])
+for l in list1:
+    if "KA" in l[3]:
+        if "except" not in l[3]:
+            print(l[0],l[1],", Occasion : ",l[2])
+    elif "except" in l[3]:
+        if "KA" not in l[3]:
+            print(l[0],l[1],", Occasion : ",l[2])
+    elif "National" in l[3]:
+        print(l[0],l[1],", Occasion : ",l[2])
